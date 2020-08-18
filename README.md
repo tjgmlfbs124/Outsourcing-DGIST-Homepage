e046171e24563b48dd004eb27cbd9fb543e6701f
TAThink <tshwang@tathink.com>
2020-08-18 19:14:42 +0900

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# KYU_Homepage_PHP
-DIGIST 학과 홈페이지 PHP 개발
--- widget/footer.php
+++
@@ -1,7 +0,0 @@
-
-<head>
-  <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/css/footer.css">
-</head>
-<footer>
-  <p>ADDRESS</p>
-</footer>
