438bbd88d19890ba792dc8d0fb3fd9801e238b0f
tjgmlfbs124 <tjgmlfbs124@gmail.com>
2020-09-09 17:20:50 +0900

    학과소개 페이지 수정

--- pg/artlab.php
+++ pg/artlab.php
@@ -14,101 +14,60 @@
   		<?php require_once $_SERVER['DOCUMENT_ROOT'].'/widget/header.php'?>
 	</header>
 
-  <!--== Start Page Header Area ==-->
-<div class="page-header-area">
-  <div class="page-header-area bg-img" data-bg="<?php $_SERVER['DOCUMENT_ROOT']?>/image/main01.jpg" style="position:absolute;"></div>
-  <div class="page-header-area bg-img" style="position:absolute; background:#080b1a; opacity: 0.7;">
-    <div class="container" style="max-width:1900px;">
-        <div class="row">
-            <div class="col-lg-10 col-xl-8 m-auto text-center">
-                <div class="page-header-content-inner">
-                    <div class="page-header-content">
-                        <h2>미래자동차연구부</h2>
-                        <p style="max-width:100%;">Global market leader of sensor convergence products <br>through radar technology Innovation</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
-    </div>
-</div>
     <div class="blog-details-page-content sp-y" style="margin-top:100px;">
       <div class="container" style="padding-bottom:40px;">
           <div class="row align-items-lg-stretch" style="align-items:stretch;">
-              <div class="col-lg-2" >
-                <p style="background:no-repeat url('<?php $_SERVER['DOCUMENT_ROOT']?>/image/sub_icon_bullet01.png'); padding-left: 14px;">개요</p>
-              </div>
               <div class="col-lg-10">
-                미래형 자동차는, 유한한 자원인 화석 연료를 사용하는 내연기관 자동차에서 전기자동차로 지속적으로 변화하고 있고, 이제는 전기자동차에서 디지털 전기자동차로, 자동차 문화에 대한 패러다임의 변화가 요구되고 있다.
-                이에 맞춰 세계최고 수준의 경쟁력 확보하기위해서는 필요한 미래형 자동차 기술을 개발하고 핵심 고급인력을 체계적으로 육성하여야 한다.
-                DGIST 미래자동차연구부는 복합기술, 실무능력, 국제감각, 경영마인드 및 창조성을 갖춘 미래 자동차 산업의 고급인력 양성을 목표,
-                복합기술, 실무능력, 국제감각, 경영마인드 및 창조성을 갖춘 미래 자동차 산업의 고급인력 양성을 목표
-              </div>
-          </div>
-
-          <div class="row align-items-lg-stretch" style="margin-top:50px;">
-              <div class="col-lg-2">
-                <p style="background:no-repeat url('<?php $_SERVER['DOCUMENT_ROOT']?>/image/sub_icon_bullet01.png'); padding-left: 14px;">설립목적</p>
-              </div>
-              <div class="col-lg-10" >
-                미래형 자동차를 위한 핵심코어 기술을 개발하고, 자동차산업 맞춤형, 학제 간 융합프로젝트에 기반한 창의적 설계교육으로 융복합기술, 국제 감각, 창조성을 갖추고 미래자동차 기술을 선도할 수 있는 핵심고급인력을 양성한다.
-                신기술 접목 및 환경 변화에 능동적으로 대처하고, 더 나아가 AI기술과 융합하여 기존 자동차기반 기술을 미래자동차 산업에 공헌 할 수 있는 인재를 양성한다.
-              </div>
-          </div>
-
-          <div class="row align-items-lg-stretch" style="margin-top:80px;">
-              <div class="col-lg-2">
-                <p style="background:no-repeat url('<?php $_SERVER['DOCUMENT_ROOT']?>/image/sub_icon_bullet01.png'); padding-left: 14px;">활동분야</p>
-              </div>
-              <div class="col-lg-2" style="top:0;">
-                <div class="author-info mt-sm-40" style="border-top:0">
-                    <div class="author-thumb">
-                        <img src="<?php $_SERVER['DOCUMENT_ROOT']?>/image/artlab-01.png" alt="Businex-Author">
-                    </div>
-                    <div class="author-txt">
-                        <h5>국방<span class="designation" style="color:#555;">국방레이더</span></h5>
-                    </div>
-                </div>
-              </div>
+                <div class="service-details-info">
+                  <div class="about-service mt-50 mt-sm-35" style="margin:0;">
+                    <h4>Mission Statements</h4>
+                    <p>We shall focus on making deep research on.</p>
+                    <ul class="service-feature">
+                        <li>unique, innovative and disruptive radar technology</li>
+                        <li>radar application technology that has great industrial impact and is tightly linked to regional strategic industries</li>
+                        <li>core breakthrough radar technology with significant depth, comprehensive academic achievements</li>
+                        <li>defense and security radar technology that reflects publicity and improves people's lives according to Korean national demands</li>
+                        <li>main research interests: Vehicle radar, biological radar, aviation and security radar, radar detection/identification and recognition technology, defense unmanned radar</li>
+                    </ul>
+                  </div>
 
 
-              <div class="col-lg-3" style="top:0;">
-                <div class="author-info mt-sm-40" style="border-top:0">
-                    <div class="author-thumb">
-                        <img src="<?php $_SERVER['DOCUMENT_ROOT']?>/image/artlab-02.png" alt="Businex-Author">
-                    </div>
-                    <div class="author-txt">
-                        <h5>자동차<span class="designation" style="color:#555;">지능형 자동차</span></h5>
-                    </div>
-                </div>
-              </div>
+                    <div class="about-service mt-50 mt-sm-35">
+                      <h4>Graduate Student Assistantship Opening</h4>
+                      <p>The Department of Interdisplinary Engineering of DGIST has an opening for the 2021 spring semester graduate student assistantship
+                        with the following research focus area (Advisor: Prof. Jonghun Leen, Email: jhlee@dgist.ac.kr):</p>
+                        <p>1) radar processing (Deep/Mahcine Learning), 2) Automotive radar, 3) Vital Radar, 4) Sensor Fusion </p>
 
-
-              <div class="col-lg-2" style="top:0;">
-                <div class="author-info mt-sm-40" style="border-top:0">
-                    <div class="author-thumb">
-                        <img src="<?php $_SERVER['DOCUMENT_ROOT']?>/image/artlab-03.png" alt="Businex-Author">
-                    </div>
-                    <div class="author-txt">
-                        <h5>항공,선박<span class="designation" style="color:#555;">항공레이더</span></h5>
-                    </div>
-                </div>
-              </div>
-
-
-              <div class="col-lg-3" style="top:0;">
-                <div class="author-info mt-sm-40" style="border-top:0">
-                    <div class="author-thumb">
-                        <img src="<?php $_SERVER['DOCUMENT_ROOT']?>/image/artlab-04.png" alt="Businex-Author">
-                    </div>
-                    <div class="author-txt">
-                        <h5>의료<span class="designation" style="color:#555;">심박측정</span></h5>
+                        <p>To qualify for this highly competitive scholarship, applicants must meet the following requirements: </p>
+                      <ul class="service-feature">
+                          <li>Hold a bachelor’s or master’s degree as of March 1st, 2021</li>
+                          <li>For master’s program (or Integrated Master’s & Doctoral program): students should hold a bachelor’s degree or a diploma that is equivalent to or higher than bachelor’s degree</li>
+                          <li>For doctoral program: students should hold a master’s degree or a diploma that is equivalent to or higher than master’s degree</li>
+                          <li>Important requirements: Official english score should be over 80 in TOEFL or 6.0 in IELTS</li>
+                          <li>main research interests: Vehicle radar, biological radar, aviation and security radar, radar detection/identification and recognition technology, defense unmanned radar</li>
+                      </ul>
+                      <p><br></p>
+                      <p>Listed below are some features of this research assistantship and the expected benefits of the program for successful applicants: </p>
+                      <ul class="service-feature">
+                          <li>Monthly allowance of at least 1,200,000 won(KRW) for PHD student and 800,000 won for master student</li>
+                          <li>Tuition fees for study period</li>
+                          <li>Extra allowances based on research performance</li>
+                      </ul>
+                      <p><br></p>
+                    <p>Application procedure: <br> Interested applicants should send the following things to the above advisor email address:</p>
+                  <ul class="service-feature">
+                    <li>Study plan (clearly stating the research topics you are interested in)</li>
+                    <li>Academic transcripts</li>
+                    <li>Brief summary of most recent thesis and the status (eg accepted, in progress, submitted, etc)</li>
+                    <li>CV (to include names, education, work experience, publications)</li>
+                    <li>Copy of official English score (TOEFL or IELTS score should be submitted)</li>
+                  </ul>
                     </div>
                 </div>
               </div>
           </div>
+        </div>
       </div>
-    </div>
 
   	<footer class="footer-area sp-y">
   			<?php require_once $_SERVER['DOCUMENT_ROOT'].'/widget/footer.php'?>
