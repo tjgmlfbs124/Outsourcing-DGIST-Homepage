b6aa64c999a583692f3def2b2a44a02c84efaf4d
tjgmlfbs124 <tjgmlfbs124@gmail.com>
2020-09-09 09:22:39 +0900

    Update footer.php

--- widget/footer.php
+++ widget/footer.php
@@ -1,24 +1,37 @@
 <!--== Start Footer Area Wrapper ==-->
-<div class="footer-widget-area sm-top-wt">
+<div class="footer-widget-area sm-top-wt" style="padding-bottom:30px;">
     <div class="container">
         <div class="row mtn-40">
-            <div class="col-md-6 col-lg">
+            <div class="col-lg-3" style="font-size:20px; text-align:center;">
               <div class="copyright-txt">
-                  <p>(우)42988 대구광역시 달성군 현풍읍 테크노중앙대로 333</p>
+                  <p style="margin:0; color:#555;" >DGIST 미래자동차연구부</p>
               </div>
+            </div>
 
-                <div class="copyright-txt">
-                  TEL :: 053-123-1234, &nbsp FAX :: 053-123-1234, &nbsp E-MAIL :: hongildong@dgist.ac.kr<br>
-                  COPYRIGHT(C) DGIST. ALL RIGHTS RESERVED.
+            <div class="col-lg-4" style="font-size:12px; text-align:center;">
+              <div class="copyright-txt">
+                <p style="margin:0; color:#555;">(우)42988 대구광역시 달성군 현풍읍 테크노중앙대로 333</p>
+                <p style="margin:0; color:#555;">COPYRIGHT(C) DGIST. ALL RIGHTS RESERVED.</p>
+              </div>
+            </div>
 
-                </div>
+            <div class="col-lg-2" style="font-size:12px; text-align:center;">
+              <div class="copyright-txt">
+                <p style="margin:0; color:#555;">Tel : 054-123-1234</p>
+                <p style="margin:0; color:#555;">Fax : 053-123-1324</p>
+              </div>
+            </div>
 
-                <div class="copyright-txt" onclick="location.href='<?php $_SERVER['DOCUMENT_ROOT']?>/pg/admin/index.php'" style="cursor:pointer;">
-                  <a style="float:right;">[관리자모드]</a>
-                </div>
-
-                <div class="copyright-txt">
-                </div>
+            <div class="col-lg-3" style="font-size:12px; text-align:center;">
+              <div class="copyright-txt">
+                  <p style="margin:0; color:#555; float:left;">관련 학부</p>
+                  <select style="margin-left:30px; color:#555; float:left;">
+                    <option>미래자동차연구소</option>
+                    <option>미래자동차융합원</option>
+                    <option>미래자동차연구부</option>
+                    <option>지능형로봇연구부</option>
+                  </select>
+              </div>
             </div>
         </div>
     </div>
